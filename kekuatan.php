b403d9efc293cdaed914c9dccb6b7c89857853ec
khansakhalda <142276767+khansakhalda@users.noreply.github.com>
2023-11-30 17:43:31 +0700

    kekuatan ke php

---
+++ kekuatan.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kekuatan Iron Man</title>
+
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Inder', sans-serif;
+        background: #000;
+        overflow-y: hidden;
+    }
+
+    .DeskripsiIronMan {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        background: black;
+    }
+
+    .UntitledDesign11 {
+        width: 900px;
+        height: 1600px;
+        left: 755px;
+        top: 47px;
+        position: absolute;
+    }
+
+    .Ellipse1 {
+        width: 100px;
+        height: 100px;
+        left: 0px;
+        top: 0px;
+        position: absolute;
+        background: rgba(217, 217, 217, 0.30);
+        border-radius: 9999px;
+    }
+
+    .Ellipse2 {
+        width: 50px;
+        height: 50px;
+        left: 25px;
+        top: 25px;
+        position: absolute;
+        background: rgba(255, 255, 255, 0.60);
+        border-radius: 9999px;
+    }
+
+    .Group1 {
+        width: 100px;
+        height: 100px;
+        left: 995px;
+        top: 797px;
+        position: absolute;
+    }
+
+    .Group2 {
+        width: 100px;
+        height: 100px;
+        left: 1325px;
+        top: 305px;
+        position: absolute;
+    }
+
+    .IronMan {
+        width: 188px;
+        height: 46px;
+        left: 626px;
+        top: 24px;
+        position: absolute;
+        color: white;
+        font-size: 40px;
+        font-weight: 400;
+        word-wrap: break-word;
+    }
+
+    .Kekuatan {
+        width: 221px;
+        height: 43px;
+        left: 42px;
+        top: 175px;
+        position: absolute;
+        color: white;
+        font-size: 30px;
+        font-weight: 400;
+        word-wrap: break-word;
+    }
+
+    .SetelahMengalamiLukaKritisSaatBertempurDenganMallenYangDiekstrakExtremisStarkMenyuntikkanSistemSarafnyaDenganMesinRestrukturisasiTubuhBerbasisTeknologiTechnoOrganikYangModisProsesEkstremisDenganMenulisUlangBiologinyaSendiriStarkMampuMenyelamatkanHidupnyaMendapatkanFaktorPenyembuhanYangDisempurnakanDanSebagianDigabungkanDenganArmorIronManMenggantikanKebutuhanAkanArmorBesarYangDikendalikanAiYangMendukungRancanganYangLebihRinganYangDikendalikanSecaraTeknisOlehOtaknyaSendiri {
+        width: 781px;
+        height: 218px;
+        left: 42px;
+        top: 229px;
+        position: absolute;
+        color: white;
+        font-size: 22px;
+        font-family: 'Jura', sans-serif;
+        font-weight: 400;
+        word-wrap: break-word;
+    }
+
+    .Rectangle3 {
+        width: 261px;
+        height: 76px;
+        left: 42px;
+        top: 533px;
+        position: absolute;
+        background: black;
+        border-radius: 30px;
+        border: 2px white solid;
+    }
+
+    .BacaSelengkapnya {
+        width: 218px;
+        height: 43px;
+        left: 70px;
+        top: 555px;
+        position: absolute;
+        color: white;
+        font-size: 23px;
+        font-weight: 400;
+        word-wrap: break-word;
+    }
+
+    .IconlyLightArrowLeftCircle {
+        width: 44px;
+        height: 44px;
+        left: 26px;
+        top: 38px;
+        position: absolute;
+    }
+
+    .ArrowLeftCircle {
+        width: 33.92px;
+        height: 33.92px;
+        left: 38.96px;
+        top: 5.04px;
+        position: absolute;
+        transform: rotate(90deg);
+        transform-origin: 0 0;
+    }
+
+    .Stroke1 {
+        width: 33.92px;
+        height: 33.92px;
+        left: 0px;
+        top: 0px;
+        position: absolute;
+        transform: rotate(90deg);
+        transform-origin: 0 0;
+        border: 1.50px white solid;
+    }
+
+    .Stroke3 {
+        width: 12.73px;
+        height: 6.39px;
+        left: -14.31px;
+        top: 10.59px;
+        position: absolute;
+        transform: rotate(90deg);
+        transform-origin: 0 0;
+        border: 1.50px white solid;
+    }
+</style>
+
+</head>
+<body>
+<div class="DeskripsiIronMan" style="width: 100%; height: 100%; position: relative; background: black">
+<a href="deskripsi.php"><img src="../assets/images/deskripsileftcircle.png" height="55px"></a>
+    <img class="UntitledDesign11" style="width: 900px; height: 1600px; left: 755px; top: 47px; position: absolute"
+        src="https://s3-alpha-sig.figma.com/img/61ca/426b/2a5069524b0f1b317efd312687b15a0d?Expires=1701648000&Signature=PSiPk1pgHSV7A3CuQtiMWYf1tFMr5rQq2LwRsciHGuECDlzNS4z1lNsjqU~a~PmnzJJMC2bdQiARLeUuTh1wClAYBs8rphz-cgw68te5JODT9SDPcK1kZlXyP25LcNorSBUgQXq68GCJ7PNgP-TY5mi8z07xslHVhdPANPxc~DC4JHyHCJ-QaqSLU7EApK3IpVWuUm5i~NsR3WF8Y2hABZK3gn2kq8FIhMpypk7ibgpdqIu9pclu7z43dEdGgltPBhNqIiSWiOvFzbuSYQw1nOY21n9bGpte1LhxGwZ5EJcXvuMtkA0aQpa-hX6FzBMmui09pX9WaFxkhBwzL7NYpA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+    <div class="Group1" style="width: 100px; height: 100px; left: 995px; top: 797px; position: absolute">
+        <div class="Ellipse1"
+            style="width: 100px; height: 100px; left: 0px; top: 0px; position: absolute; background: rgba(217, 217, 217, 0.30); border-radius: 9999px">
+        </div>
+        <div class="Ellipse2"
+            style="width: 50px; height: 50px; left: 25px; top: 25px; position: absolute; background: rgba(255, 255, 255, 0.60); border-radius: 9999px">
+        </div>
+    </div>
+    <div class="Group2" style="width: 100px; height: 100px; left: 1325px; top: 305px; position: absolute">
+        <div class="Ellipse1"
+            style="width: 100px; height: 100px; left: 0px; top: 0px; position: absolute; background: rgba(217, 217, 217, 0.30); border-radius: 9999px">
+        </div>
+        <div class="Ellipse2"
+            style="width: 50px; height: 50px; left: 25px; top: 25px; position: absolute; background: rgba(255, 255, 255, 0.60); border-radius: 9999px">
+        </div>
+    </div>
+    <div class="Group1" style="width: 200px; height: 200px; left: 795px; top: 176px; position: absolute">
+        <div class="Ellipse1"
+            style="width: 200px; height: 200px; left: 0px; top: 0px; position: absolute; background: rgba(217, 217, 217, 0.30); border-radius: 9999px">
+        </div>
+        <div class="Ellipse2"
+            style="width: 100px; height: 100px; left: 50px; top: 50px; position: absolute; background: rgba(255, 255, 255, 0.60); border-radius: 9999px">
+        </div>
+    </div>
+    <div class="IronMan"
+        style="width: 188px; height: 46px; left: 626px; top: 24px; position: absolute; color: white; font-size: 35px; font-family: Inder; font-weight: 400; word-wrap: break-word">
+        IRON MAN</div>
+    <div class="Kekuatan"
+        style="width: 221px; height: 43px; left: 42px; top: 175px; position: absolute; color: white; font-size: 30px; font-family: Inder; font-weight: 400; word-wrap: break-word">
+        KEKUATAN</div>
+    <div class="SetelahMengalamiLukaKritisSaatBertempurDenganMallenYangDiekstrakExtremisStarkMenyuntikkanSistemSarafnyaDenganMesinRestrukturisasiTubuhBerbasisTeknologiTechnoOrganikYangModisProsesEkstremisDenganMenulisUlangBiologinyaSendiriStarkMampuMenyelamatkanHidupnyaMendapatkanFaktorPenyembuhanYangDisempurnakanDanSebagianDigabungkanDenganArmorIronManMenggantikanKebutuhanAkanArmorBesarYangDikendalikanAiYangMendukungRancanganYangLebihRinganYangDikendalikanSecaraTeknisOlehOtaknyaSendiri"
+        style="width: 781px; height: 218px; left: 42px; top: 229px; position: absolute; color: white; font-size: 22px; font-family: Jura; font-weight: 400; word-wrap: break-word">
+        Setelah mengalami luka kritis saat bertempur dengan Mallen yang diekstrak Extremis, Stark menyuntikkan sistem
+        sarafnya dengan mesin restrukturisasi tubuh berbasis teknologi techno-organik yang modis (proses
+        Ekstremis).<br /><br />Dengan menulis ulang biologinya sendiri, Stark mampu menyelamatkan hidupnya, mendapatkan
+        faktor penyembuhan yang disempurnakan, dan sebagian digabungkan dengan armor Iron Man, menggantikan kebutuhan
+        akan armor besar yang dikendalikan AI yang mendukung rancangan yang lebih ringan, yang dikendalikan secara
+        teknis oleh otaknya sendiri.<br /><br /></div>
+    <a href="https://id.wikipedia.org/wiki/Iron_Man"><div class="Rectangle3"
+        style="width: 261px; height: 76px; left: 42px; top: 465px; position: absolute; background: black; border-radius: 30px; border: 2px white solid">
+    </div></a>
+    <div class="BacaSelengkapnya"
+        style="width: 218px; height: 43px; left: 75px; top: 490px; position: absolute; color: white; font-size: 23px; font-family: Inder; font-weight: 400; word-wrap: break-word">
+        <a href="https://id.wikipedia.org/wiki/Iron_Man">Baca Selengkapnya</a></div>
+    </div>
+</div>
+</body>
+
+</html>
