a4f9617cad6d57052855de4411c512295ded92bf
Mut127 <muthiakhanza034@gmail.com>
2023-11-30 16:57:54 +0700

    manambahkan 2 funtion pada signin
    menambahkan function redirectToSignIn($errorCode) dan function authenticateUser($conn, $username, $password)  dalam autentikasi

--- autentikasi.php
+++ autentikasi.php
@@ -1,22 +1,37 @@
 <?php
 session_start();
 include ('koneksi.php');
-$username = $_POST ['username'];
-$password = $_POST ['password'];
 
-$sql = "SELECT * from akun where username='$username' and password='$password'";
-$query = mysqli_query ($conn,$sql);
-if(mysqli_num_rows($query)==1){
-  header ('Location:daftarfilm.php');
-  $user = mysqli_fetch_array($query);
-  $_SESSION['username'] = $user['username'];
-  $_SESSION['role'] = $user['role'];
-}
-else if($username == '' || $password ==''){
-    header ('Location:signin.php?error=2');
-}
-else{
-    header ('Location:signin.php?error=1');
+function redirectToSignIn($errorCode) {
+    $location = 'signin.php';
+    if ($errorCode) {
+        $location .= '?error=' . $errorCode;
+    }
+    header("Location: $location");
+    exit();
 }
 
+function authenticateUser($conn, $username, $password) {
+    $sql = "SELECT * FROM akun WHERE username='$username' AND password='$password'";
+    $query = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($query) == 1) {
+        $user = mysqli_fetch_array($query);
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+        header('Location: daftarfilm.php');
+        exit();
+    } elseif ($username == '' || $password == '') {
+        redirectToSignIn(2); // Error code 2: Username or password is empty
+    } else {
+        redirectToSignIn(1); // Error code 1: Invalid username or password
+    }
+}
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    authenticateUser($conn, $username, $password);
+}
 ?>
